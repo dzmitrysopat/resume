de5ff48b4f35f22f128e685c1cd47c5664e7387d
Dzmitry Sopat <dzmitrysopat@gmail.com>
2024-02-24 15:20:21 +0300

    main

--- script-new.js
+++ script-new.js
@@ -1,16 +1,23 @@
 function nextStep2(){
     document.getElementById("commonInfo").classList.add("hidden");
     document.getElementById("contactInfo").classList.remove("hidden");
+    document.getElementById("firstBlock").classList.remove("active");
+    document.getElementById("secondBlock").classList.add("active");
 }
 
 function nextStep3(){
     document.getElementById("contactInfo").classList.add("hidden");
     document.getElementById("educationInfo").classList.remove("hidden");
+    document.getElementById("secondBlock").classList.remove("active");
+    document.getElementById("thirdBlock").classList.add("active");
+
 }
 
 function nextStep4(){
     document.getElementById("educationInfo").classList.add("hidden");
     document.getElementById("otherInfo").classList.remove("hidden");
+    document.getElementById("thirdBlock").classList.remove("active");
+    document.getElementById("lastBlock").classList.add("active");
 }
 
 // prev Steps
@@ -18,14 +25,20 @@ function nextStep4(){
 function prevStep1(){
     document.getElementById("commonInfo").classList.remove("hidden");
     document.getElementById("contactInfo").classList.add("hidden");
+    document.getElementById("firstBlock").classList.add("active");
+    document.getElementById("secondBlock").classList.remove("active");
 }
 
 function prevStep2(){
     document.getElementById("educationInfo").classList.add("hidden");
     document.getElementById("contactInfo").classList.remove("hidden");
+    document.getElementById("thirdBlock").classList.remove("active");
+    document.getElementById("secondBlock").classList.add("active");
 }
 
 function prevStep3(){
     document.getElementById("otherInfo").classList.add("hidden");
     document.getElementById("educationInfo").classList.remove("hidden");
+    document.getElementById("lastBlock").classList.remove("active");
+    document.getElementById("thirdBlock").classList.add("active");
 }
