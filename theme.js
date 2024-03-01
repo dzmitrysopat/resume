245e1d2dc878ee27b64c0ccb7659a969bf0e8d6d
Dzmitry Sopat <dzmitrysopat@gmail.com>
2024-03-01 08:27:21 +0300

    main

---
+++ theme.js
@@ -0,0 +1,28 @@
+
+document.querySelector('.theme').addEventListener('click', (event) => {
+    event.preventDefault();
+    if (localStorage.getItem('theme') === 'dark') {
+      localStorage.removeItem('theme');
+    }
+    else {
+      localStorage.setItem('theme', 'dark')
+    }
+    addDarkClassToHTML()
+  });
+
+  function addDarkClassToHTML() {
+    const image = document.getElementById("logoImg");
+
+    try {
+      if (localStorage.getItem('theme') === 'dark') {
+        document.querySelector('html').classList.add('dark');
+        image.src = './img/bulb_sm_white.png';
+      }
+      else {
+        document.querySelector('html').classList.remove('dark');
+        image.src = './img/bulb_sm.png';
+      }
+    } catch (err) { }
+  }
+  
+  addDarkClassToHTML();
