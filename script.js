70b7c5418a72fae459b8398fdec37d8d1eb3fedb
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2024-09-29 22:03:51 +0300

    main

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const file_reader = new FileReader();
     file_reader.onload = () => response(file_reader.result);
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const TOKEN = '7437568340:AAGGMMdXb_FcoGW__PZ5L858HqjKTU1CKNw'
+    const CHAT_ID = '-1002433455867'
+    const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+    // const photo_API = `https://api.telegram.org/bot${TOKEN}/sendDocument`
+
+
 // input_file.addEventListener('change', async function(){
 //     const file = document.querySelector("#photo").files;
 //     const my_image = await convert_to_base64(file[0]);
@@ -94,6 +101,66 @@ function prevStep3(){
     // document.getElementById("lastBlock").classList.remove("active");
     // document.getElementById("thirdBlock").classList.add("active");
 }
+
+function clearInputs(inputs) {
+    inputs.forEach((input) => input.value = '')
+  }
+
+  document.getElementById('telegramForm').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const date = new Date().toLocaleString()
+
+    const textMessage = `
+      <b>Анкета | ${date}</b>\n
+      <b>ФИО: </b>${this.name.value}
+      <b>Дата рождения: </b>${this.birth.value}
+      <b>Семейное положение: </b>${this.family.value}
+      <b>Гражданство: </b>${this.country.value}
+      <b>Город: </b>${this.city.value}
+      <b>Телефон: </b><a href="tel:${this.tel.value}">${this.tel.value}</a>
+      <b>Эл.Почта: </b><a href="mailto:${this.email.value}">${this.email.value}</a>
+      <b>Telegram: </b><a href="${this.telegram.value}">${this.telegram.value}</a>
+      <b>Instagram: </b><a href="${this.instagram.value}">${this.instagram.value}</a>
+      <b>Учебное заведение: </b>${this.college.value}
+      <b>Уровень образования: </b>${this.education.value}
+      <b>Форма обучения: </b>${this.educationType.value}
+      <b>Специальность: </b>${this.speciality.value}
+      <b>Год поступления: </b>${this.startYear.value}
+      <b>Год окончания: </b>${this.finishYear.value}
+      <b>Тема выпускной работы: </b>${this.diploma.value}
+      <b>Языки: </b>${this.languages.value}
+      <b>Навыки: </b>${this.skills.value}
+      <b>Что заинтересовало: </b>${this.interestings.value}
+      <b>Достижения: </b>${this.achievements.value}
+      <b>О себе: </b><i>${this.about.value}</i>
+    `
+
+    if (document.getElementById("languages").value.length ==   0) {
+      alert('Укажите языки');
+  } else if (document.getElementById("skills").value.length == 0){
+      alert('Ваши скиллы для успеха')
+  } else if (document.getElementById("interestings").value.length == 0){
+      alert('Чем заинтересовал проект')
+  } else if (document.getElementById("achievements").value.length == 0){
+      alert('Ваши достижения')    
+  }   else if (document.getElementById("agree").checked == false) {
+    alert('Нам нужно Ваше согласие на обработку получаемых данных')
+  } else  
+  {
+    fetch(URI_API, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json', },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        parse_mode: 'html',
+        text: textMessage
+      }),
+    })
+      .then(() => open('./sent.html'))
+      .catch(e => console.error(e))
+      .finally(() => clearInputs(this.querySelectorAll('input, select, textarea')))
+  }})
+})
 
 document.querySelector('.theme').addEventListener('click', (event) => {
     event.preventDefault();
